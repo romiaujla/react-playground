3bd0f10bf9957b2eaa04c4b1884a5deacee1d291
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-07-12 10:18:53 -0400

    added sections to Tooltip

--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Tooltip(props){
     return (
-        <span className="Tooltip"></span>
+        <span className="Tooltip">
+            <span className="Tooltip-content">
+                {props.children}
+            </span>
+            <span className="Tooltip-message">
+                {props.message}
+            </span>
+        </span>
     );
 }
 
