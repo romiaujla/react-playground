6d576d3a349eccc5b1bd156930febc21acf951ba
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-07-12 08:56:42 -0400

    Creates a separate component Split

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Split from './composition/Split';
+import './App.css';
 
 function App() {
   return (
     <main className="App">
-      
+      <Split className="left" flexBasis={2}>
+        This is the content for the left `Split`.
+      </Split>
+      <Split className="right">
+        This is the content for the right `Split`.
+      </Split>
     </main>
   );
 }
