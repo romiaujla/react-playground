710a862796b639dd2c28767946a686073b057f2f
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-07-25 19:25:06 -0400

    Created Bomb Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HelloWorld />
+        <Bomb boomTiming={8} className='tick-tock'/>
       </div>
       
     );
--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -3,14 +3,61 @@ import './Bomb.css';
 
 class Bomb extends React.Component {
 
+    static defaultProps ={
+        boomTiming: 8
+    }
+
     constructor(props){
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            text: `tick`
         }
     }
 
-    
+    // Starts Updating the component every second
+    componentDidMount(){
+        // console.log(`component mounted with count: `, this.state.count);
+        this.interval = setInterval(() => {
+            
+            this.setState({
+                count: this.state.count+1
+            });
+            
+            const count = this.state.count;
+            if(count%2 === 1){
+                this.setState({
+                    text: `tock`
+                });
+            }else{
+                this.setState({
+                    text: `tick`
+                });
+            }
+            
+            // uses the prop on when to BOOM
+            if(count >= this.props.boomTiming){
+                this.setState({
+                    text: `BOOM!`
+                });
+                clearInterval(this.interval);
+            }
+
+            // console.log(this.state.count, this.state.text);
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+
+    render(){
+        return (
+            <div className='tick-tock'>
+                <p>{this.state.text}</p>
+            </div>
+        );  
+    }
 
 }
 
