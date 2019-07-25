9b120fc66b2bc78e431ffb86589233b435efa6c7
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-07-25 18:53:33 -0400

    Created HelloWorld Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from './state/Counter';
+import HelloWorld from './state-drills/HelloWorld';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        <HelloWorld />
       </div>
       
     );
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Bomb.css';
+
+class Bomb extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    
+
+}
+
+export default Bomb;
